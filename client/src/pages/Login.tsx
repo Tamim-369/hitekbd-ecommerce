6b17a692f4a650f1d189da56e32e0ffccbc1ff7f
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-12-01 04:01:37 -0500

    fixing login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -92,19 +92,6 @@ export default function Login() {
           </button>
         </div>
       </form>
-
-      <div className="mt-6">
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-300" />
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-white text-gray-500">
-              Or continue with
-            </span>
-          </div>
-        </div>
-      </div>
     </AuthLayout>
   );
 }
