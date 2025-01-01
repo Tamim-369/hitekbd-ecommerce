9ec2f94649a2ab62dbd3770fca3392098a2e3c07
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-12-31 21:46:41 -0500

    fixing update product issue

--- client/src/data/baseApi.ts
+++ client/src/data/baseApi.ts
@@ -1,2 +1,4 @@
-export const ApiURL: string = 'https://api.hitekbd.com/api/v1';
-export const ImageURL: string = 'https://api.hitekbd.com';
+export const ApiURL: string =
+  import.meta.env.VITE_API_URL || 'https://api.hitekbd.com/api/v1';
+export const ImageURL: string =
+  import.meta.env.VITE_IMAGE_URL || 'https://api.hitekbd.com';
