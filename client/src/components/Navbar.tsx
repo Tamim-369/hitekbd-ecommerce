c2716ff80ceba07ba6924aa5b8c0172a05d1fdb1
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-02-16 02:41:45 -0500

    fixing some buttons

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { Search, ShoppingCart, User, Menu, LogOut, UserCircle, User2 } from 'lucide-react';
+import { Search, ShoppingCart, User, Menu, LogOut, UserCircle, User2, SearchXIcon, FileSearch, TextSearch } from 'lucide-react';
 import { useState } from 'react';
 import { useCart } from '../contexts/CartContext';
 import { useAuth } from '../contexts/AuthContext';
@@ -35,13 +35,13 @@ export default function Navbar() {
                 type="text"
                 name="search"
                 placeholder="Search products..."
-                className="w-[30rem] px-6 py-3 rounded-full border border-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                className="w-[14rem] min-[877px]:w-[20rem] lg:w-[30rem] px-6 py-3 rounded-full border border-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
               />
               <Search className="absolute right-5 top-3 text-gray-400 h-6 w-6" />
             </form>
           </div>
 
-          <div className="hidden lg:flex items-center space-x-6">
+          <div className="flex items-center sm:space-x-6 space-x-2.5">
             <Link
               to="/cart"
               className="relative p-2 rounded-full bg-gradient-to-r from-[#37c3fa]/10 to-[#ce62f2]/10 transition-colors duration-200 group"
@@ -63,7 +63,7 @@ export default function Navbar() {
 
                 <Link
                   to="/profile"
-                  className="relative group flex items-center gap-2 px-4 py-2 rounded-xl font-medium text-white transition-all duration-300 bg-gradient-to-r from-[#37c3fa] to-[#ce62f2]  hover:shadow-lg hover:shadow-indigo-500/25 hover:scale-105 active:scale-95"
+                  className="relative group flex items-center gap-2 px-2.5 py-2.5 sm:px-4 sm:py-2 rounded-full sm:rounded-xl font-medium text-white transition-all duration-300 bg-gradient-to-r from-[#37c3fa] to-[#ce62f2]  hover:shadow-lg hover:shadow-indigo-500/25 hover:scale-105 active:scale-95"
                 >
                   <div className="relative flex items-center">
                     <User
@@ -71,7 +71,7 @@ export default function Navbar() {
                       className="group-hover:rotate-6 transition-transform duration-200"
                     />
                   </div>
-                  <span className="text-sm">Profile</span>
+                  <span className="text-sm sm:block hidden">Profile</span>
 
                   <div className="absolute inset-0 rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 blur-xl -z-10" />
                 </Link>
@@ -86,14 +86,17 @@ export default function Navbar() {
                 <span>Login</span>
               </Link>
             )}
-
+            <div className="md:hidden">
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="flex justify-center items-center p-3  rounded-full bg-gradient-to-r from-[#37c3fa]/10 to-[#c937fb]/10 hover:from-[#37c3fa]/20 hover:to-[#c937fb]/20 active:from-[#37c3fa]/30 active:to-[#c937fb]/30 shadow-sm hover:shadow transition-all duration-200"
+              >
+                <Search className="h-5 w-5 text-gray-700" />
+              </button>
+            </div>
           </div>
 
-          <div className="lg:hidden">
-            <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-              <Menu className="h-8 w-8 text-gray-600" />
-            </button>
-          </div>
+
         </div>
       </div>
 
@@ -110,52 +113,8 @@ export default function Navbar() {
               />
               <Search className="absolute right-5 top-3 text-gray-400 h-6 w-6" />
             </form>
-            {isAuthenticated ? (
-              <>
-                <Link
-                  to="/profile"
-                  className="text-gray-600 hover:text-indigo-600 flex items-center gap-2 w-full py-2"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  <User size={20} />
-                  <span>Profile</span>
-                </Link>
-                {/* <button
-                  onClick={() => {
-                    logout();
-                    setIsMenuOpen(false);
-                  }}
-                  className="text-gray-600 hover:text-indigo-600 flex items-center gap-2 w-full py-2"
-                >
-                  <LogOut size={20} />
-                  Logout
-                </button> */}
-              </>
-            ) : (
-              <Link
-                to="/login"
-                className="text-gray-600 hover:text-indigo-600 flex items-center gap-2 w-full py-2"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                <User size={20} />
-                <span>Login</span>
-              </Link>
-            )}
-            <Link
-              to="/cart"
-              className="text-gray-600 hover:text-indigo-600 flex items-center gap-2 w-full py-2"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              <div className="relative">
-                <ShoppingCart size={20} />
-                {totalItems > 0 && (
-                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-4 h-4 rounded-full flex items-center justify-center">
-                    {totalItems}
-                  </span>
-                )}
-              </div>
-              <span>Cart</span>
-            </Link>
+
+
           </div>
         </div>
       )}
