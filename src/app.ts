d8c6c3ced462557f654a475285949975823b0f64
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-02-21 06:17:00 -0500

    tring to fix

--- src/app.ts
+++ src/app.ts
@@ -40,6 +40,10 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
