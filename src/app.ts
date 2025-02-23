1d0a6d6f92f3c0baf0154a809be6e43122bbdd2d
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-02-23 02:54:20 -0500

    fixing some issues

--- src/app.ts
+++ src/app.ts
@@ -40,8 +40,8 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //file retrieve
 app.use(express.static('uploads'));
